2a718938065f98bde43e31a1204dd3e70ed9b459
brsh <brsh@users.noreply.github.com>
2020-01-10 12:38:47 -0800

    Built Group & Unique into Get-SubnetsWithoutSites

--- public/sites.ps1
+++ public/sites.ps1
@@ -11,9 +11,21 @@ function Get-adSubnetsWithoutSite {
 	roaming profiles, lots of GPOs, netlogon scripts....
 
 	This script parses the netlogon log file (%SystemRoot%\debug\netlogon.log)
-	and breaks out any system/ip info for the error 'no client site'
+	and breaks out any system/ip info for the error 'no client site' - and
+	because of that: * It Needs to Be Run On a DC * (and possy all of them).
 
-	I recommend piping this to "Sort-Object IP -Unique" or "Group-Object IP"
+	<strikethru>I recommend piping this to "Sort-Object IP -Unique" or
+	"Group-Object IP"</strikethru>
+
+	Well, I did recommend that, but now I've just built that in ... please
+	note that using these switches mean the output is buffered into those
+	commands :)
+
+	.PARAMETER Group
+	Pipes the output to Group-Object by IP (also sorts, but not unique)
+
+	.PARAMETER Unique
+	Pipes the output to Sort-Object -Unique by IP
 
 	.EXAMPLE
 	Get-adSubnetsWithoutSite
@@ -26,26 +38,61 @@ function Get-adSubnetsWithoutSite {
 	Sorts the list of "siteless" Systems down to unique IPs
 
 	.EXAMPLE
+	Get-adSubnetsWithoutSite -Unique
+
+	Sorts the list of "siteless" Systems down to unique IPs
+
+	.EXAMPLE
 	Get-adSubnetsWithoutSite | Group-Object IP
 
 	Groups the list of "siteless" Systems by IPs
+
+	.EXAMPLE
+	Get-adSubnetsWithoutSite -Group
+
+	Groups the list of "siteless" Systems by IPs
+
 	#>
+	[CmdletBinding(DefaultParameterSetName = 'Unique')]
+	param (
+		[Parameter(ParameterSetName = 'Group')]
+		[switch] $Group = $false,
+		[Parameter(ParameterSetName = 'Unique')]
+		[switch] $Unique = $false
+	)
 
 	[string] $IPLog = "$($env:SystemRoot)\debug\netlogon.log"
-	if (Test-Path $IPLog ) {
-		Get-Content $IPLog | ForEach-Object {
-			$a = [string] $_
-			if ($a -match 'NO_CLIENT_SITE') {
-				New-Object -TypeName PSCustomObject -Property @{
-					Date   = $a.Split(' ')[0]
-					Time   = $a.Split(' ')[1]
-					System = $a.Split(' ')[-2]
-					IP     = $a.SPlit(' ')[-1]
+	$All = @()
+	try {
+		if (Test-Path $IPLog -ErrorAction Stop) {
+			Get-Content $IPLog | ForEach-Object {
+				$a = [string] $_
+				if ($a -match 'NO_CLIENT_SITE') {
+					$NoClient = New-Object -TypeName PSCustomObject -Property @{
+						Date   = $a.Split(' ')[0]
+						Time   = $a.Split(' ')[1]
+						System = $a.Split(' ')[-2]
+						IP     = $a.Split(' ')[-1]
+					}
+					switch ($true) {
+						$Group { $All += $NoClient; break }
+						$Unique { $All += $NoClient; break }
+						DEFAULT { $NoClient; break }
+					}
 				}
 			}
+			if ($All.Count -gt 0) {
+				switch ($true) {
+					$Group { $All | Sort-Object IP | Group-Object IP; break }
+					$Unique { $All | Sort-Object IP -Unique; break }
+				}
+			}
+		} else {
+			Write-Status -Message "Could not find file:", $IPLog -Type Error, Warning -Level 1
+			Write-Status -Message 'Unfortunately, this needs to run on a DC (at the moment)' -Type Info -Level 2
 		}
-	} else {
-		Write-Status -Message "Could not find file:", $IPLog -Type Error, Warning -Level 1
+	} catch {
+		Write-Status -Message "Weird... an error occurred. Maybe you're not an admin?" -Type Error -E $_ -Level 1
 	}
 }
 
